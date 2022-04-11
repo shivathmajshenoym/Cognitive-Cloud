63105492437ace61af4c3a71666d1f1114952946
SHIVATHMAJ SHENOY M <54990364+shivathmajshenoym@users.noreply.github.com>
2022-04-11 07:02:05 +0530

    Create SimpleCalci.rb

---
+++ Section2/SimpleCalci.rb
@@ -0,0 +1,14 @@
+user=10
+user2=20
+
+math ="add"
+case math
+when "add"
+    puts user + user2
+when "subtract"
+    puts user - user2
+when "multiply"
+    puts user * user2
+else
+    puts "I don't understand"
+end
